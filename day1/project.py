5f05411c5bd085cb5230d2f46b4961a18e396f86
Yuchi Li <sjtiantang@gmail.com>
2022-02-19 22:23:54 -0800

    complete project

---
+++ day1/project.py
@@ -0,0 +1,7 @@
+import time
+
+print("This program gives a suggestion of your band's name")
+time.sleep(1)
+a = input("Tell me the city that you grew up in:\n")
+b = input("What is your pet's name:\n")
+print("Suggestion of your band's name: " + a + " " + b)
